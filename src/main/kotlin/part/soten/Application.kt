041e553bfed3d4baac6cf8dec96908482c9ca7de
ChoiYounho <maxosa78@gmail.com>
2023-08-25 15:38:16 +0900

    feat: hello world 찍기

--- src/main/kotlin/part/soten/Application.kt
+++ src/main/kotlin/part/soten/Application.kt
@@ -3,11 +3,18 @@ package part.soten
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import part.soten.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-            .start(wait = true)
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
 }
 
 fun Application.module() {
