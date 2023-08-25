e4ff8a2ab50f478d56f85aa96f4f206605976cbb
ChoiYounho <maxosa78@gmail.com>
2023-08-25 17:53:55 +0900

    feat: usres table

---
+++ src/main/kotlin/part/soten/user/Users.kt
@@ -0,0 +1,8 @@
+package part.soten.user
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+object Users : IntIdTable() {
+    val name = varchar("name", 255)
+    val email = varchar("email", 255)
+}
