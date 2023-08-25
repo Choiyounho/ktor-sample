dd58dcbb9bbddc292f79057e0eac02202ffb5e30
ChoiYounho <maxosa78@gmail.com>
2023-08-25 17:54:05 +0900

    feat: db 셋팅

--- src/main/kotlin/part/soten/Application.kt
+++ src/main/kotlin/part/soten/Application.kt
@@ -1,22 +1,39 @@
 package part.soten
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import part.soten.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import part.soten.plugins.configureRouting
+import part.soten.plugins.userRouting
+import part.soten.user.Users
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!")
-            }
-        }
+        module()
     }.start(wait = true)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    initDatabase()
+
     configureRouting()
+    userRouting()
 }
+
+fun initDatabase() {
+    Database.connect("jdbc:h2:./test", driver = "org.h2.Driver")
+    transaction {
+        SchemaUtils.create(Users)
+    }
+}
+
+
