573a15ab0c9e1ce84b1eb4ba8e10c3598083ca14
ChoiYounho <maxosa78@gmail.com>
2023-08-25 17:44:38 +0900

    샘플 수정

--- src/main/kotlin/part/soten/Sample.kt
+++ src/main/kotlin/part/soten/Sample.kt
@@ -1,6 +1,9 @@
 package part.soten
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Sample(
     val name: String = "최윤호",
-    val age: Int = 28
+    val email: String = "assd"
 )
