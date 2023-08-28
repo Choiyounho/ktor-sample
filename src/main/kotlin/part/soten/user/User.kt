54858e64177fa17003ad3f591ae7640f22034ad8
ChoiYounho <maxosa78@gmail.com>
2023-08-28 18:43:38 +0900

    refactor: Entity Int -> Long

--- src/main/kotlin/part/soten/user/User.kt
+++ src/main/kotlin/part/soten/user/User.kt
@@ -2,11 +2,12 @@ package part.soten.user
 
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import part.soten.Users
 
-class User(id: EntityID<Int>) : IntEntity(id) {
-    companion object : IntEntityClass<User>(Users)
+class User(id: EntityID<Long>) : LongEntity(id) {
+    companion object : LongEntityClass<User>(Users)
 
     var name by Users.name
     var email by Users.email
